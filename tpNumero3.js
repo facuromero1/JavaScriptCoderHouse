18fd1f07b3f506f34e38cb32f9864f4fa5cad3bf
facuromero1 <79130935+facuromero1@users.noreply.github.com>
2022-05-12 15:34:03 -0300

    Add files via upload

---
+++ tpNumero3.js
@@ -0,0 +1,30 @@
+const Container = require("./project/contenedor");
+const express = require("express");
+
+const port = 8080;
+
+const app = express();
+const containerOne = new Container("objetos.txt");
+
+
+const ConectionServer = app.listen(port,(req,res) =>{
+    console.log(`server is litening on port: ${ConectionServer.address().port}`)
+})
+
+
+app.get("/productos",(req,res) =>{
+    totalProducts = containerOne.getAllItems()
+    res.send({ totalProducts });
+})
+
+
+app.get("/productoRandom",(req,res) =>{
+  randomProduct = containerOne.getRandom()
+  res.send(randomProduct);
+})
+
+
+
+
+
+
